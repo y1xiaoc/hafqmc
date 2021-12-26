e7307420aff782ede2a173d6713b564ffd243a82
Yixiao Chen <yixiaoc@princeton.edu>
2021-12-25 22:24:42 -0500

    simple hmc sampler

--- sampler.py
+++ sampler.py
@@ -8,9 +8,9 @@ from .ansatz import BraKet
 from .utils import PyTree, Array
 
 
-def log_dens_gaussian(x, mu=0., sigma=1.):
+def logd_gaussian(x, mu=0., sigma=1.):
     """unnormalized log density of Gaussian distribution"""
-    return -0.5 * ((x - mu) / sigma) ** 2
+    return -0.5 * jnp.abs((x - mu) / sigma) ** 2
 
 
 def ravel_shape(target_shape):
@@ -72,12 +72,14 @@ def make_sampler(braket: BraKet, name: str, **kwargs):
 
 def choose_sampler_maker(name: str) -> Callable[..., MCSampler]:
     name = name.lower()
-    if name == "gaussian":
+    if name in ("direct", "gaussian"):
         return make_gaussian
     if name in ("metropolis", "mcmc", "mh"):
         return make_metropolis
     if name in ("langevin", "mala"):
         return make_langevin
+    if name in ("hamiltonian", "hybrid", "hmc"):
+        return make_hamiltonian
     raise NotImplementedError(f"unsupported sampler type: {name}")
 
 
@@ -94,7 +96,7 @@ def make_gaussian(logdens_fn, fields_shape, mu=0., sigma=1., truncate=None):
         else:
             rawgs = jax.random.normal(key, shape)
         new_fields = rawgs * sigma + mu
-        new_logdens = log_dens_gaussian(new_fields, mu, sigma).sum(-1)
+        new_logdens = logd_gaussian(new_fields, mu, sigma).sum(-1)
         return state, (batch_unravel(new_fields), new_logdens)
     
     def init(key, params, batch_size):
@@ -155,7 +157,7 @@ def make_langevin(logdens_fn, fields_shape, beta=1., tau=0.01, steps=5):
     def step(key, params, state):
         x1, g1, ld1 = state
         gkey, ukey = jax.random.split(key)
-        x2 = x1 + tau * g1 + jnp.sqrt(2*tau) * jax.random.normal(gkey, shape=x1.shape)
+        x2 = x1 + tau*g1 + jnp.sqrt(2*tau)*jax.random.normal(gkey, shape=x1.shape)
         ld2, g2 = logd_and_grad(params, x2)
         g2 = g2.conj() # handle complex grads, no influence for real case
         ratio = ld2 + log_q(x1, x2, g2) - ld1 - log_q(x2, x1, g1)
@@ -179,3 +181,66 @@ def make_langevin(logdens_fn, fields_shape, beta=1., tau=0.01, steps=5):
         return (fields, grads_new.conj(), ld_new)
 
     return MCSampler(sample, init, refresh)
+
+
+def make_hamiltonian(logdens_fn, fields_shape, beta=1., dt=0.1, length=1.):
+    fsize, unravel = ravel_shape(fields_shape)
+    ravel_logd = lambda p, x: beta * logdens_fn(p, unravel(x))
+    batch_unravel = jax.vmap(unravel, in_axes=0)
+    batch_logd = jax.vmap(ravel_logd, in_axes=(None, 0))
+    logd_and_grad = jax.vmap(jax.value_and_grad(ravel_logd, 1), in_axes=(None, 0))
+
+    def sample(key, params, state):
+        gkey, ukey = jax.random.split(key)
+        q1, f1, ld1 = state
+        p1 = jax.random.normal(gkey, shape=q1.shape)
+        grad_fn = jax.vmap(jax.grad(lambda x: -ravel_logd(params, x)))
+        q2, p2, f2 = leapfrog_carry(q1, p1, f1, grad_fn, dt, round(length / dt))
+        ld2 = batch_logd(params, q2)
+        ratio = (logd_gaussian(-p2).sum(-1)+ld2) - (logd_gaussian(p1).sum(-1)+ld1)
+        (qn, fn, ldn), accepted = mh_select(ukey, ratio, state, (q2, f2, ld2))
+        return (qn, fn, ldn), (batch_unravel(qn), ldn)
+
+    def init(key, params, batch_size):
+        sigma, mu = 1., 0.
+        fields = jax.random.normal(key, (batch_size, fsize)) * sigma + mu
+        logdens, grads = logd_and_grad(params, fields)
+        return (fields, grads.conj(), logdens)
+
+    def refresh(state, params):
+        fields, grads_old, ld_old = state
+        ld_new, grads_new = logd_and_grad(params, fields)
+        return (fields, grads_new.conj(), ld_new)
+
+    return MCSampler(sample, init, refresh)
+
+
+def leapfrog(q, p, grad_fn, dt, steps):
+    # simple Euler integration step
+    def int_step(carry, _):
+        q, p = carry
+        q += dt * p
+        p -= dt * grad_fn(q)
+        return (q, p), None
+    # leapfrog by shifting half step
+    p -= 0.5 * dt * grad_fn(q) # first half p
+    (q, p), _ = lax.scan(int_step, (q, p), None, length=steps-1)
+    q += dt * p # final whole step update of q
+    p -= 0.5 * dt * grad_fn(q) # final half p
+    return q, p
+
+
+def leapfrog_carry(q, p, f, grad_fn, dt, steps):
+    # simple Euler integration step
+    def int_step(carry, _):
+        q, p = carry
+        q += dt * p
+        p -= dt * grad_fn(q)
+        return (q, p), None
+    # leapfrog by shifting half step
+    p += 0.5 * dt * f # first half p
+    (q, p), _ = lax.scan(int_step, (q, p), None, length=steps-1)
+    q += dt * p # final whole step update of q
+    f = -grad_fn(q) # force at final point
+    p += 0.5 * dt * f # final half p
+    return q, p, f
